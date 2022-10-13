c69edba9e8dbc3c3882a78c0632ae9419a82bdc0
Olha Krasikova <okrasikova09@gmail.com>
2022-10-13 14:49:08 +0200

    Initial commit

---
+++ src/main/java/com/example/springdatawebapp/SpringDataWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataWebAppApplication.class, args);
+    }
+
+}
